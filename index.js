9f0c9b01e476d15646bbcfe490fe887edd72232b
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-06-24 23:52:01 +0200

    Lookup weather application server side completely built

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import router from './router.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = 5000;
+
+
+app.use('/weather/', router);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  const d = new Date();
+  res.json({currentTime: d.toTimeString()});
+  console.log('Received a GET request');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
---
+++ router.js
@@ -0,0 +1,75 @@
+import express, {Router} from 'express';
+import axios from 'axios';
+import 'dotenv/config';
+
+let router = Router();
+
+router.get('/:location', async (req, response) => {
+  const location = req.params.location;
+  let image;
+  let weatherData;
+  let error;
+
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY_WEATHER}&units=metric`;
+    weatherData = await axios(url);
+  } catch (err) {
+    console.log(err.message);
+    error = err;
+  }
+
+  if (weatherData) {
+    let photos = await getImage(weatherData.data.name);
+    if (photos.length > 0) {
+      image = photos[0].src.landscape;
+    } else {
+      photos = await getImage(weatherData.data.sys.country);
+      if (photos.length > 0) {
+        image = photos[0].src.landscape;
+      } else {
+        image = '';
+      }
+    }
+  }
+
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  if (error) response.send({error});
+  else response.send({data: weatherData.data, image});
+});
+
+async function getImage(query) {
+  try {
+    const cityImageRes = await axios(
+      `https://api.pexels.com/v1/search?query=${query}`,
+      {
+        headers: {
+          Authorization: process.env.API_KEY_IMAGE,
+        },
+      },
+    );
+    return cityImageRes.data.photos;
+  } catch (err) {
+    console.log(err.message);
+    return '';
+  }
+}
+
+router.get('/forecast/:location', async (req, response) => {
+  const location = req.params.location;
+  let weatherData;
+  let error;
+
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${process.env.API_KEY_WEATHER}&units=metric`;
+    weatherData = await axios(url);
+  } catch (err) {
+    console.log(err);
+    error = err;
+  }
+
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  if (error) response.send({error});
+  else response.send(weatherData.data);
+});
+
+export default router;
