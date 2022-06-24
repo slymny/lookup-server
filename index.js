45180da59b77a0ae18edd9a243e8237a0bc095ab
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-06-25 00:52:01 +0200

    bug fixed

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
 
 app.use('/weather/', router);
 
